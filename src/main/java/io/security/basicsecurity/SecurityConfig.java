33e113dd984f0eb8de864b377d660bec9295b48f
youngkim90 <ceo.youngkim@gmail.com>
2022-02-06 23:46:45 +0900

    SecurityConfig 설정

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,24 @@
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //인증
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated();
+        //인가
+        http
+                .formLogin();
+
+    }
+}
